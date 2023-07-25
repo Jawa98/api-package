4403ef4c325e392c982060ee3a4a61f46cd0d8bf
Jawa <shriba.jawa5@gmail.com>
2023-07-25 12:05:41 +0300

    Fix in upload_file function

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\PersonalAccessToken;
 
@@ -24,6 +25,10 @@ if(!function_exists('upload_file'))
     {
         $extension = $request_file->getClientOriginalExtension();
         $file_to_store = $prefix . '_' . time(). rand(1000, 9999) . '.' . $extension;
+        $path = storage_path('app/public/'. $folder_name);
+        if(!File::isDirectory($path))
+            File::makeDirectory($path, 0755, true, true);
+
         $request_file->storeAs('public/' . $folder_name, $file_to_store);
         return $folder_name.'/'.$file_to_store;
     }
